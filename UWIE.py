3987d1e8cf869ca8055e093ec68892bcac2fd873
Venkata Kartheek Karnati <47190727+Kartheek77@users.noreply.github.com>
2019-10-18 19:21:57 +0530

    Update UWIE.py

--- UWIE.py
+++ UWIE.py
@@ -1,3 +1,8 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
+GivenImage = cv2.imread("SampleImage1.jpg")
+GivenImage = cv2.cvtColor(GivenImage, cv2.COLOR_BGR2RGB)
+plt.imshow(GivenImage)
+plt.show()
