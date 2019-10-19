b1925d8d6386638da7ab40951d7853df1502d11a
Venkata Kartheek Karnati <47190727+Kartheek77@users.noreply.github.com>
2019-10-19 07:27:53 +0530

    Update UWIE.py

--- UWIE.py
+++ UWIE.py
@@ -1,24 +1,15 @@
 import cv2
-import numpy as np
+import numpy as np 
 import matplotlib.pyplot as plt
+%matplotlib inline
 
-GivenImage = cv2.imread("SampleImage1.jpg")
-GivenImage = cv2.cvtColor(GivenImage, cv2.COLOR_BGR2RGB)
-plt.imshow(GivenImage)
-plt.show()
-
-RedChannel = GivenImage[:,:,0]
-GreenChannel = GivenImage[:,:,1]
-BlueChannel = GivenImage[:,:,2] 
-
-fobj(w,im):
+def objectiveFunction(w,image):
     #compute the histogram
     TotalNumberOfPixelsInImage = (image.shape[0])*(image.shape[1]) 
     NumberofPixelsPerIntensity = np.zeros(256)
     for i in range(0,256):
         NumberofPixelsPerIntensity[i] = np.sum(image==i)
     probDensity = NumberofPixelsPerIntensity/TotalNumberOfPixelsInImage
-    
     #from w compute the lower and upper limits
     lowerLimit = 0
     upperLimit = 255
@@ -34,21 +25,58 @@ fobj(w,im):
         if(tempSum>w):
             upperLimit = int(255-i)
             break
-    
     #perform contrast stretching to map lower and upper limits to a and b
     a = 0
     b = 255
     factor = ((b-a)/(upperLimit-lowerLimit))
-    transformedImage = np.round_(a+((im-lowerLimit)*factor))
-    
+    transformedImage = np.round_(a+((im-lowerLimit)*factor))    
     #compute the histogram of transformed image
     NumberofPixelsPerIntensity = np.zeros(256)
     for i in range(0,256):
         NumberofPixelsPerIntensity[i] = np.sum(transformedImage==i)
     NewprobDensity = NumberofPixelsPerIntensity/TotalNumberOfPixelsInImage
-    
     #compute the entropy
     entropy = -1*np.sum(NewprobDensity*np.log2(NewprobDensity))
-    
-    
-    
+    #compute the average gradient
+    dy,dx = np.gradient(transformedImage)
+    avg_gradient = np.sum(np.sqrt((dy*dy)+(dx*dx)))/TotalNumberOfPixelsInImage
+    #return sum of these
+    return entropy+avg_gradient
+
+def contrastStretching(image,w):
+    #compute the histogram
+    TotalNumberOfPixelsInImage = (image.shape[0])*(image.shape[1]) 
+    NumberofPixelsPerIntensity = np.zeros(256)
+    for i in range(0,256):
+        NumberofPixelsPerIntensity[i] = np.sum(image==i)
+    probDensity = NumberofPixelsPerIntensity/TotalNumberOfPixelsInImage
+    #from w compute the lower and upper limits
+    lowerLimit = 0
+    upperLimit = 255
+    tempSum = 0
+    for i in range(0,256):
+        tempSum += probDensity[i]
+        if(tempSum>w):
+            lowerLimit = i
+            break
+    tempSum = 0
+    for i in range(0,256):
+        tempSum += probDensity[int(255-i)]
+        if(tempSum>w):
+            upperLimit = int(255-i)
+            break
+    #perform contrast stretching to map lower and upper limits to a and b
+    a = 0
+    b = 255
+    factor = ((b-a)/(upperLimit-lowerLimit))
+    transformedImage = np.round_(a+((im-lowerLimit)*factor))
+    return transformedImage
+
+def EnhanceTheImage(GivenImage):
+    BestWforEachChannel = np.zeros(3)
+    for i in range(0,3):
+        BestWforEachChannel[i] = DEAlgo
+    EnhancedImage = np.zeros(GivenImage)
+    for i in range(0,3):
+        EnhancedImage[:,:,i] = contrastStretching(GivenImage[:,:,i],BestWforEachChannel[i])
+    return EnhancedImage        
